442fc61d1bbcfb308fb25721a4b2b78aff247ec2
Toni Carrasco <toni.carrasco@adevinta.com>
2025-05-29 09:38:43 +0200

    tune llama-7b model for lora setup

--- utils.py
+++ utils.py
@@ -106,12 +106,7 @@ def load_model(model_name, hf_token, quantization_config = None):
     elif model_name == 'llama-7b':
         model_kwargs["token"] = hf_token
         if quantization_config is None:
-            model_kwargs["quantization_config"] = BitsAndBytesConfig(
-                                                              load_in_4bit=True,
-                                                              bnb_4bit_use_double_quant=True,
-                                                              bnb_4bit_quant_type="nf4",
-                                                              bnb_4bit_compute_dtype=torch.bfloat16
-                                                          )
+            model_kwargs["torch_dtype"] = torch.float16
         return AutoModelForCausalLM.from_pretrained('meta-llama/Llama-2-7b-hf', **model_kwargs)
 
     else:
