8640beb832fbe729684b9ebda04ba580e0960b90
Ivan <ivan.molera@adevinta.com>
2025-06-03 09:05:17 +0200

    Static dataset and prompt build (#16)
    * infer build prompt
    
    * static dataset

--- src/infer.py
+++ src/infer.py
@@ -1,24 +1,19 @@
 import torch
 from transformers import AutoTokenizer
 from peft import PeftModel
-from utils import parse_args, load_env_vars, get_model_config, load_model
+from utils import parse_args, load_env_vars, get_model_config, load_model, load_peft_arguments_from_json
+from wikisql_dataset import get_tokenizer
+from train import get_qlora_quantification_config
 
 def main():
     args = parse_args()
     env = load_env_vars()
     config = get_model_config(args.model, args.peft)
-
-    tokenizer = AutoTokenizer.from_pretrained(config.hf_name, token=env.hf_token)
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.pad_token_id = tokenizer.eos_token_id
+    tokenizer = get_tokenizer(env.hf_token, config)
 
     if args.peft == "qlora":
-        bnb_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_quant_type='nf4',
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_compute_dtype=torch.float16
-        )
+        peft_config = load_peft_arguments_from_json("configs/peft_qlora_configuration.json", config.output_dir)
+        bnb_config = get_qlora_quantification_config(peft_config)
     else:
         bnb_config = None
 
@@ -31,10 +26,23 @@ def main():
 
     print("Escribe un prompt o '/quit' para salir.")
     while True:
-        prompt = input(">> ")
-        if prompt.strip() == "/quit":
+        question = input("Question >> ")
+        if question.strip() == "/quit":
             break
-        inputs = tokenizer(prompt, return_tensors='pt', padding=True, truncation=True, max_length=512).to(device)
+
+        columns = input("Columns (comma‐separated) >> ")
+        columns = ", ".join([col.strip() for col in columns.split(",") if col.strip()])
+
+        combined_prompt = f"Question: {question}\nColumns: {columns}\nSQL:"
+
+        inputs = tokenizer(
+            combined_prompt,
+            return_tensors="pt",
+            padding=True,
+            truncation=True,
+            max_length=512
+        ).to(device)
+
         outputs = peft_model.generate(
             **inputs,
             max_new_tokens=100,
@@ -44,6 +52,7 @@ def main():
             do_sample=True,
             pad_token_id=tokenizer.eos_token_id
         )
+
         print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 
 if __name__ == '__main__':
