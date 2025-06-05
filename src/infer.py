236437e153d8712137214b5385eadb75ebb2650b
Toni Carrasco <toni.carrasco@adevinta.com>
2025-06-05 14:11:32 +0200

    Add perform test

--- src/infer.py
+++ src/infer.py
@@ -1,9 +1,77 @@
 import torch
+import time
 from transformers import AutoTokenizer
 from peft import PeftModel
 from utils import parse_args, load_env_vars, get_model_config, load_model, load_peft_arguments_from_json
-from wikisql_dataset import get_tokenizer
+from wikisql_dataset import get_wikisql_datasets, get_tokenizer
 from train import get_qlora_quantification_config
+
+
+def outputs_match(expected, inferred):
+    def normalize(text):
+        return " ".join(text.lower().split())
+    return normalize(expected) == normalize(inferred)
+
+
+def infer(combined_prompt, tokenizer, peft_model, device):
+
+    inputs = tokenizer(
+        combined_prompt,
+        return_tensors="pt",
+        padding=True,
+        truncation=True,
+        max_length=512 # TODO: Aixo esta definit com a constant?
+    ).to(device)
+
+    outputs = peft_model.generate(
+        **inputs,
+        max_new_tokens=100,
+        temperature=0.7,
+        top_k=50,
+        top_p=0.9,
+        do_sample=True,
+        pad_token_id=tokenizer.eos_token_id
+    )
+
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+
+def test_prompt(input_prompt, expected_output, tokenizer, peft_model, device):
+
+    start_time = time.time()
+    inferred_output = infer(input_prompt, tokenizer, peft_model, device)
+    end_time = time.time()
+
+    elapsed_time = end_time - start_time
+    match = outputs_match(expected_output, inferred_output)
+
+    if not match:
+        print("== fail")
+        print(f"prompt: {input_prompt}")
+        print(f"expected: {expected_output}")
+        print(f"inferred: {inferred_output}")
+
+    return elapsed_time, match
+
+def perform_test(tokenizer, peft_model, device, hf_token, dataset_size_ratio):
+    # Dataset
+    train_dataset, eval_dataset = get_wikisql_datasets(tokenizer, hf_token, dataset_size_ratio)
+    prompts = train_dataset["input_ids"]
+    sqls = train_dataset["labels"]
+
+    total_time = 0.0
+    match_count = 0
+    mismatch_count = 0
+
+    for i, (p, s) in enumerate(zip(prompts, sqls), 1):
+        elapsed, match = test_prompt(p, s, tokenizer, peft_model, device)
+        total_time += elapsed
+
+    print("=== Resultados globales ===")
+    print(f"Total inference time: {total_time:.2f} seconds")
+    print(f"Matches:   {match_count}")
+    print(f"Mismatches:{mismatch_count}")
+
 
 def main():
     args = parse_args()
@@ -29,31 +97,16 @@ def main():
         question = input("Question >> ")
         if question.strip() == "/quit":
             break
+        elif question == "/test":
+            perform_test(tokenizer, peft_model, device, env.hf_token, env.dataset_size_ratio)
+            break
 
         columns = input("Columns (comma‐separated) >> ")
         columns = ", ".join([col.strip() for col in columns.split(",") if col.strip()])
-
         combined_prompt = f"Question: {question}\nColumns: {columns}\nSQL:"
 
-        inputs = tokenizer(
-            combined_prompt,
-            return_tensors="pt",
-            padding=True,
-            truncation=True,
-            max_length=512
-        ).to(device)
-
-        outputs = peft_model.generate(
-            **inputs,
-            max_new_tokens=100,
-            temperature=0.7,
-            top_k=50,
-            top_p=0.9,
-            do_sample=True,
-            pad_token_id=tokenizer.eos_token_id
-        )
-
-        print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+        inferred_output = infer(combined_prompt, tokenizer, peft_model, device)
+        print(inferred_output)
 
 if __name__ == '__main__':
     main()
